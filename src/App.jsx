63f2e5a05b8c43b28067698c1f6e95d1244697ca
Obaid-ur-Rehman <obaidurrehman555@gmail.com>
2024-07-08 13:18:10 +0500

    added assets constants and created a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
+import Header from './components/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <h1 className='font-bold underline'>Hello Friend</h1>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+      <ButtonGradient />
     </>
   )
 }
