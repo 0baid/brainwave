b24345396e9523308743d30bbd1fae0b9bda44c2
Obaid-ur-Rehman <obaidurrehman555@gmail.com>
2024-07-09 10:07:12 +0500

    starting working on section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = ({className,id,crosses,crossesOffset,customPadding,children}) => {
+  return (
+    <div className={`relative ${customPadding || `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40' : ''} ${className || ''}` }`}>
+        {children}
+    </div>
+  )
+}
+
+export default Section
